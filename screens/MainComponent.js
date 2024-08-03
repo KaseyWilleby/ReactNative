b34e158ffbd8454e3f557ff5dab11769db9c342e
Kasey Willeby <kaseywilleby@gmaill.com>
2024-08-03 04:07:57 -0500

    Reservation Form and Modal Commit

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -15,6 +15,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
+import ReservationScreen from './ReservationScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -94,6 +95,31 @@ const ContactNavigator = () => {
     );
 };
 
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return(
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Reservation'
+                component={ReservationScreen}
+                options={({ navigation }) => ({
+                    title:'Reservation Search',
+                    headerLeft: () => (
+                        <Icon
+                            name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
+
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
     return (
@@ -163,7 +189,8 @@ const Main = () => {
             <Drawer.Navigator
                 initialRouteName='HomeNav'
                 drawerContent={CustomDrawerContent}
-                drawerStyle ={{ backgroundColor: '#CEC8FF'}}
+                screenOptions ={{drawerStyle: {
+                     backgroundColor:'#CEC8FF'}}}
             >
                 <Drawer.Screen
                     name='HomeNav'
@@ -223,6 +250,23 @@ const Main = () => {
                         drawerIcon: ({ color }) => (
                             <Icon
                                 name='address-card'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    
+                    }}
+                />
+                <Drawer.Screen
+                    name='ReserveCampsite'
+                    component={ReservationNavigator}
+                    options={{ 
+                        title: 'Reserve Campsite',
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='tree'
                                 type='font-awesome'
                                 size={24}
                                 iconStyle={{ width: 24 }}
