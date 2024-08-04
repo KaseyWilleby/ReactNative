f7d698bbf3e1457aa0646737ab03046fde2764b5
Kasey Willeby <kaseywilleby@gmaill.com>
2024-08-04 11:31:33 -0500

    Post Workshop #2 Commit

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -15,10 +15,25 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comment/postComment',
+    async (payload, {dispatch, getState}) => {
+        setTimeout(()=> {
+            const {comments} = getState();
+            payload.date = new Date().toISOString();
+            payload.id =  comments.commentsArray.length;
+            dispatch(addComment(payload));
+        },2000)
+    }
+);
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+        state.commentsArray.push(action.payload)
+    }},
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -39,3 +54,4 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+export const {addComment} = commentsSlice.actions;
