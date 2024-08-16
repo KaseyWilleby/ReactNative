98b37a590e288c046f0bfb57e4d9c9fa413bed4f
Kasey Willeby <kaseywilleby@gmaill.com>
2024-08-16 01:11:00 -0500

    Social media and email commit

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,4 @@
-import { StyleSheet,Text, View, PanResponder, Alert } from "react-native";
+import { StyleSheet,Text, View, PanResponder, Alert, Share } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable';
@@ -50,6 +50,19 @@ const RenderCampsite = (props) => {
         
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+            title, 
+            message: `${title}: ${message} ${url}`,
+            url
+            },
+            {
+                dialogTitle: 'Share' + title
+            }
+        );
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -87,12 +100,28 @@ const RenderCampsite = (props) => {
                             reverse
                             onPress = {() => props.onShowModal()}
                         />
+                        <Icon 
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress = {() => 
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
+                        />
                     </View>
                 </Card> 
             </Animatable.View>
             
         );
     }
+    
+
     return <View />;
 };
 
